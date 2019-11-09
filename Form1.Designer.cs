60baeae2a9d56023bd94b80886ca164a374db84e
FriendlyFFF <Victor_Hemphill@HotMail.com>
2019-11-08 16:49:35 -0800

    First draft of TimeCard
    Files uploaded from C:\Users\v-SteHem.Redmond\source\repos\TimeCard\

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace TimeCard
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSShift = new System.Windows.Forms.Label();
+            this.lblSBreak1 = new System.Windows.Forms.Label();
+            this.lblEBreak1 = new System.Windows.Forms.Label();
+            this.lblSLunch = new System.Windows.Forms.Label();
+            this.lblELunch = new System.Windows.Forms.Label();
+            this.lblSBreak2 = new System.Windows.Forms.Label();
+            this.lblEBreak2 = new System.Windows.Forms.Label();
+            this.lblEShift = new System.Windows.Forms.Label();
+            this.tbxSShift = new System.Windows.Forms.TextBox();
+            this.tbxSBreak1 = new System.Windows.Forms.TextBox();
+            this.tbxEBreak1 = new System.Windows.Forms.TextBox();
+            this.tbxSLunch = new System.Windows.Forms.TextBox();
+            this.tbxELunch = new System.Windows.Forms.TextBox();
+            this.tbxSBreak2 = new System.Windows.Forms.TextBox();
+            this.tbxEBreak2 = new System.Windows.Forms.TextBox();
+            this.tbxEShift = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSShift
+            // 
+            this.lblSShift.AutoSize = true;
+            this.lblSShift.Location = new System.Drawing.Point(77, 59);
+            this.lblSShift.Name = "lblSShift";
+            this.lblSShift.Size = new System.Drawing.Size(81, 20);
+            this.lblSShift.TabIndex = 1;
+            this.lblSShift.Text = "Start Shift";
+            this.lblSShift.Click += new System.EventHandler(this.lblSShift_Click);
+            // 
+            // lblSBreak1
+            // 
+            this.lblSBreak1.AutoSize = true;
+            this.lblSBreak1.Location = new System.Drawing.Point(77, 119);
+            this.lblSBreak1.Name = "lblSBreak1";
+            this.lblSBreak1.Size = new System.Drawing.Size(103, 20);
+            this.lblSBreak1.TabIndex = 2;
+            this.lblSBreak1.Text = "Start Break 1";
+            this.lblSBreak1.Click += new System.EventHandler(this.lblSBreak1_Click);
+            // 
+            // lblEBreak1
+            // 
+            this.lblEBreak1.AutoSize = true;
+            this.lblEBreak1.Location = new System.Drawing.Point(429, 119);
+            this.lblEBreak1.Name = "lblEBreak1";
+            this.lblEBreak1.Size = new System.Drawing.Size(97, 20);
+            this.lblEBreak1.TabIndex = 3;
+            this.lblEBreak1.Text = "End Break 1";
+            this.lblEBreak1.Click += new System.EventHandler(this.lblEBreak1_Click);
+            // 
+            // lblSLunch
+            // 
+            this.lblSLunch.AutoSize = true;
+            this.lblSLunch.Location = new System.Drawing.Point(77, 192);
+            this.lblSLunch.Name = "lblSLunch";
+            this.lblSLunch.Size = new System.Drawing.Size(92, 20);
+            this.lblSLunch.TabIndex = 4;
+            this.lblSLunch.Text = "Start Lunch";
+            this.lblSLunch.Click += new System.EventHandler(this.lblSLunch_Click);
+            // 
+            // lblELunch
+            // 
+            this.lblELunch.AutoSize = true;
+            this.lblELunch.Location = new System.Drawing.Point(429, 192);
+            this.lblELunch.Name = "lblELunch";
+            this.lblELunch.Size = new System.Drawing.Size(86, 20);
+            this.lblELunch.TabIndex = 5;
+            this.lblELunch.Text = "End Lunch";
+            this.lblELunch.Click += new System.EventHandler(this.lblELunch_Click);
+            // 
+            // lblSBreak2
+            // 
+            this.lblSBreak2.AutoSize = true;
+            this.lblSBreak2.Location = new System.Drawing.Point(77, 261);
+            this.lblSBreak2.Name = "lblSBreak2";
+            this.lblSBreak2.Size = new System.Drawing.Size(103, 20);
+            this.lblSBreak2.TabIndex = 6;
+            this.lblSBreak2.Text = "Start Break 2";
+            this.lblSBreak2.Click += new System.EventHandler(this.lblSBreak2_Click);
+            // 
+            // lblEBreak2
+            // 
+            this.lblEBreak2.AutoSize = true;
+            this.lblEBreak2.Location = new System.Drawing.Point(429, 261);
+            this.lblEBreak2.Name = "lblEBreak2";
+            this.lblEBreak2.Size = new System.Drawing.Size(97, 20);
+            this.lblEBreak2.TabIndex = 7;
+            this.lblEBreak2.Text = "End Break 2";
+            this.lblEBreak2.Click += new System.EventHandler(this.lblEBreak2_Click);
+            // 
+            // lblEShift
+            // 
+            this.lblEShift.AutoSize = true;
+            this.lblEShift.Location = new System.Drawing.Point(429, 335);
+            this.lblEShift.Name = "lblEShift";
+            this.lblEShift.Size = new System.Drawing.Size(75, 20);
+            this.lblEShift.TabIndex = 8;
+            this.lblEShift.Text = "End Shift";
+            this.lblEShift.Click += new System.EventHandler(this.lblEShift_Click);
+            // 
+            // tbxSShift
+            // 
+            this.tbxSShift.Location = new System.Drawing.Point(214, 52);
+            this.tbxSShift.Name = "tbxSShift";
+            this.tbxSShift.Size = new System.Drawing.Size(175, 26);
+            this.tbxSShift.TabIndex = 9;
+            // 
+            // tbxSBreak1
+            // 
+            this.tbxSBreak1.Location = new System.Drawing.Point(214, 119);
+            this.tbxSBreak1.Name = "tbxSBreak1";
+            this.tbxSBreak1.Size = new System.Drawing.Size(175, 26);
+            this.tbxSBreak1.TabIndex = 10;
+            // 
+            // tbxEBreak1
+            // 
+            this.tbxEBreak1.Location = new System.Drawing.Point(576, 119);
+            this.tbxEBreak1.Name = "tbxEBreak1";
+            this.tbxEBreak1.Size = new System.Drawing.Size(175, 26);
+            this.tbxEBreak1.TabIndex = 11;
+            // 
+            // tbxSLunch
+            // 
+            this.tbxSLunch.Location = new System.Drawing.Point(214, 192);
+            this.tbxSLunch.Name = "tbxSLunch";
+            this.tbxSLunch.Size = new System.Drawing.Size(175, 26);
+            this.tbxSLunch.TabIndex = 12;
+            // 
+            // tbxELunch
+            // 
+            this.tbxELunch.Location = new System.Drawing.Point(576, 189);
+            this.tbxELunch.Name = "tbxELunch";
+            this.tbxELunch.Size = new System.Drawing.Size(175, 26);
+            this.tbxELunch.TabIndex = 13;
+            // 
+            // tbxSBreak2
+            // 
+            this.tbxSBreak2.Location = new System.Drawing.Point(214, 261);
+            this.tbxSBreak2.Name = "tbxSBreak2";
+            this.tbxSBreak2.Size = new System.Drawing.Size(175, 26);
+            this.tbxSBreak2.TabIndex = 14;
+            // 
+            // tbxEBreak2
+            // 
+            this.tbxEBreak2.Location = new System.Drawing.Point(576, 258);
+            this.tbxEBreak2.Name = "tbxEBreak2";
+            this.tbxEBreak2.Size = new System.Drawing.Size(175, 26);
+            this.tbxEBreak2.TabIndex = 15;
+            // 
+            // tbxEShift
+            // 
+            this.tbxEShift.Location = new System.Drawing.Point(576, 335);
+            this.tbxEShift.Name = "tbxEShift";
+            this.tbxEShift.Size = new System.Drawing.Size(175, 26);
+            this.tbxEShift.TabIndex = 16;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(677, 492);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 30);
+            this.btnQuit.TabIndex = 18;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.tbxEShift);
+            this.Controls.Add(this.tbxEBreak2);
+            this.Controls.Add(this.tbxSBreak2);
+            this.Controls.Add(this.tbxELunch);
+            this.Controls.Add(this.tbxSLunch);
+            this.Controls.Add(this.tbxEBreak1);
+            this.Controls.Add(this.tbxSBreak1);
+            this.Controls.Add(this.tbxSShift);
+            this.Controls.Add(this.lblEShift);
+            this.Controls.Add(this.lblEBreak2);
+            this.Controls.Add(this.lblSBreak2);
+            this.Controls.Add(this.lblELunch);
+            this.Controls.Add(this.lblSLunch);
+            this.Controls.Add(this.lblEBreak1);
+            this.Controls.Add(this.lblSBreak1);
+            this.Controls.Add(this.lblSShift);
+            this.Name = "frm1";
+            this.Text = "TimeCard";
+            this.Load += new System.EventHandler(this.frm1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSShift;
+        private System.Windows.Forms.Label lblSBreak1;
+        private System.Windows.Forms.Label lblEBreak1;
+        private System.Windows.Forms.Label lblSLunch;
+        private System.Windows.Forms.Label lblELunch;
+        private System.Windows.Forms.Label lblSBreak2;
+        private System.Windows.Forms.Label lblEBreak2;
+        private System.Windows.Forms.Label lblEShift;
+        private System.Windows.Forms.TextBox tbxSShift;
+        private System.Windows.Forms.TextBox tbxSBreak1;
+        private System.Windows.Forms.TextBox tbxEBreak1;
+        private System.Windows.Forms.TextBox tbxSLunch;
+        private System.Windows.Forms.TextBox tbxELunch;
+        private System.Windows.Forms.TextBox tbxSBreak2;
+        private System.Windows.Forms.TextBox tbxEBreak2;
+        private System.Windows.Forms.TextBox tbxEShift;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeCard
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblSShift_Click(object sender, EventArgs e)
+        {
+            tbxSShift.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            lblSBreak1.Visible = true;
+            tbxSBreak1.Visible = true;
+            lblSLunch.Visible = true;
+            tbxSLunch.Visible = true;
+            lblEShift.Visible = true;
+            tbxEShift.Visible = true;
+        }
+
+        // When form loads, only 'Start Shift' option should be visible.
+        // =============================================================
+        private void frm1_Load(object sender, EventArgs e)
+        {
+            lblSBreak1.Visible = false;
+            tbxSBreak1.Visible = false;
+            lblEBreak1.Visible = false;
+            tbxEBreak1.Visible = false;
+            lblSLunch.Visible  = false;
+            tbxSLunch.Visible  = false;
+            lblELunch.Visible  = false;
+            tbxELunch.Visible  = false;
+            lblSBreak2.Visible = false;
+            tbxSBreak2.Visible = false;
+            lblEBreak2.Visible = false;
+            tbxEBreak2.Visible = false;
+            lblEShift.Visible  = false;
+            tbxEShift.Visible  = false;
+        }
+
+        private void lblSBreak1_Click(object sender, EventArgs e)
+        {
+            tbxSBreak1.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            lblEBreak1.Visible = true;
+            tbxEBreak1.Visible = true;
+            lblSLunch.Visible = false;
+            tbxSLunch.Visible = false;
+            lblEShift.Visible = false;
+            tbxEShift.Visible = false;
+        }
+
+        private void lblEBreak1_Click(object sender, EventArgs e)
+        {
+            tbxEBreak1.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            lblSLunch.Visible = true;
+            tbxSLunch.Visible = true;
+            lblEShift.Visible = true;
+            tbxEShift.Visible = true;
+        }
+
+        private void lblSLunch_Click(object sender, EventArgs e)
+        {
+            tbxSLunch.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            if (tbxSBreak1.Text == "")
+            {
+                lblSBreak1.Visible = false;
+                tbxSBreak1.Visible = false;
+                lblEBreak1.Visible = false;
+                tbxEBreak1.Visible = false;
+            }
+            lblELunch.Visible = true;
+            tbxELunch.Visible = true;
+            lblEShift.Visible = false;
+            tbxEShift.Visible = false;
+        }
+
+        private void lblELunch_Click(object sender, EventArgs e)
+        {
+            tbxELunch.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            lblSBreak2.Visible = true;
+            tbxSBreak2.Visible = true;
+            lblEShift.Visible = true;
+            tbxEShift.Visible = true;
+        }
+
+        private void lblSBreak2_Click(object sender, EventArgs e)
+        {
+            tbxSBreak2.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            lblEBreak2.Visible = true;
+            tbxEBreak2.Visible = true;
+            lblEShift.Visible = false;
+            tbxEShift.Visible = false;
+        }
+
+        private void lblEBreak2_Click(object sender, EventArgs e)
+        {
+            tbxEBreak2.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            lblEShift.Visible = true;
+            tbxEShift.Visible = true;
+        }
+
+        private void lblEShift_Click(object sender, EventArgs e)
+        {
+            tbxEShift.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            if (tbxSBreak2.Text == "")
+            {
+                lblSBreak2.Visible = false;
+                tbxSBreak2.Visible = false;
+                lblEBreak2.Visible = false;
+                tbxEBreak2.Visible = false;
+            }
+        }
+    }
+}
